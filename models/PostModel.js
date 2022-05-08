0b08149c5f10b4e33c613fb0f35b78905b514b85
harold <harold@weibyapps.com>
2022-05-08 18:16:37 +0800

    fix: 移除version

--- models/PostModel.js
+++ models/PostModel.js
@@ -1,37 +1,42 @@
 const mongoose = require('mongoose')
 
-const PostSchema = new mongoose.Schema({
-  ownerAccount: {
-    type: String,
-    required: [true, '貼文者帳號未填寫'],
+const PostSchema = new mongoose.Schema(
+  {
+    ownerAccount: {
+      type: String,
+      required: [true, '貼文者帳號未填寫'],
+    },
+    ownerName: {
+      type: String,
+      required: [true, '貼文姓名未填寫'],
+    },
+    content: {
+      type: String,
+      required: [true, 'Content 未填寫'],
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    createdAt: {
+      type: Date,
+      default: '',
+      select: true,
+    },
+    updatedAt: {
+      type: Date,
+      default: '',
+      select: true,
+    },
   },
-  ownerName: {
-    type: String,
-    required: [true, '貼文姓名未填寫'],
-  },
-  content: {
-    type: String,
-    required: [true, 'Content 未填寫'],
-  },
-  image: {
-    type: String,
-    default: '',
-  },
-  likes: {
-    type: Number,
-    default: 0,
-  },
-  createdAt: {
-    type: Date,
-    default: '',
-    select: true,
-  },
-  updatedAt: {
-    type: Date,
-    default: '',
-    select: true,
-  },
-})
+  {
+    versionKey: false,
+  }
+)
 
 const Post = mongoose.model('Post', PostSchema)
 
